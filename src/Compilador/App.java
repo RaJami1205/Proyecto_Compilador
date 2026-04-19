dc3d625c8fd9c3ca9db473d2fe28c3975506e12c
RaJami1205 <rauljamiralfarorodriguez512@gmail.com>
2026-04-19 13:55:14 -0600

    Cambio del nombre en el compilador

--- src/Compilador/App.java
+++ src/Compilador/App.java
@@ -8,7 +8,7 @@ import java_cup.runtime.Symbol;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 
-public class TestCompilador {
+public class App {
 
     public static void main(String[] args) {
         String mode;
