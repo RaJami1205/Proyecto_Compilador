ae21b48723727a66bd2c4c6e2aa36388723a5f36
Diego <daraya0906@gmail.com>
2026-04-25 11:48:12 -0600

    Archivo - tokens

--- src/Compilador/ReporteCompilador.java
+++ src/Compilador/ReporteCompilador.java
@@ -22,4 +22,30 @@ public class ReporteCompilador {
             List<String> erroresSintacticos,
             boolean esValido
     ) throws IOException {
+        try (PrintWriter pw = new PrintWriter(
+                new OutputStreamWriter(new FileOutputStream(archivoSalida), "UTF-8"))) {
+
+            pw.println("REPORTE DE ANÁLISIS - COMPILADOR");
+            pw.println();
+            pw.println("Archivo fuente : " + archivoFuente);
+            pw.println();
+
+            // SECCIÓN 1: TOKENS
+            pw.println("==========================================");
+            pw.println(" SECCIÓN 1: TOKENS ENCONTRADOS");
+            pw.println("==========================================");
+            pw.printf("%-6s %-6s %-25s %-25s%n",
+                    "Línea", "Col", "Token", "Lexema");
+            pw.println("-".repeat(65));
+
+            for (Symbol token : tokens) {
+                String tokenName = sym.terminalNames[token.sym];
+                String lexema    = token.value != null ? token.value.toString() : tokenName;
+                pw.printf("%-6d %-6d %-25s %-25s%n",
+                        token.left, token.right, tokenName, lexema);
+            }
+
+            pw.println();
+        }
+    }
 }
